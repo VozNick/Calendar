a84998decba12938fb9f01cfb695c650b8775e3e
VozNick <vmm408@gmail.com>
2017-04-23 20:40:54 +0300

    add notification

--- app/src/main/java/com/example/vmm408/calendarmar_29/service/NotificationService.java
+++ app/src/main/java/com/example/vmm408/calendarmar_29/service/NotificationService.java
@@ -1,5 +1,8 @@
 package com.example.vmm408.calendarmar_29.service;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -7,12 +10,18 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationCompat;
+
+import com.example.vmm408.calendarmar_29.MainActivity;
+import com.example.vmm408.calendarmar_29.R;
 
 public class NotificationService extends Service {
     private BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
             System.out.println("alert !!!");
+//            startForeground(111, createNotification());
+            createNotification();
         }
     };
 
@@ -26,6 +35,21 @@ public class NotificationService extends Service {
     public int onStartCommand(Intent intent, int flags, int startId) {
         registerReceiver(broadcastReceiver, new IntentFilter("wakeup"));
         return super.onStartCommand(intent, flags, startId);
+    }
+
+    private void createNotification() {
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 112, new Intent(this, MainActivity.class), Intent.FILL_IN_ACTION);
+        NotificationCompat.Builder notification = new NotificationCompat.Builder(this);
+
+        notification.setSmallIcon(R.drawable.ic_sms_black_24dp);
+        notification.setContentTitle("My title");
+        notification.setContentText("My description");
+        notification.setAutoCancel(true);
+        notification.setContentIntent(pendingIntent);
+        notification.addAction(new NotificationCompat.Action(0, "OKi", pendingIntent));
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.notify(111, notification.build());
     }
 
     @Override
